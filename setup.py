6aa8f5036ae28c79657d6ec99059a096d9bec00f
Boaz Mohar <boazmohar@gmail.com>
2017-10-12 10:22:40 -0400

    develop: Changed README to .rst format

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from codecs import open
 from os import path
 here = path.abspath(path.dirname(__file__))
 
 
-with open(path.join(here, 'README.txt'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
